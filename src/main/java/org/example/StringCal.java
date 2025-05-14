cf2d0b42e36b06027637bedc6641057f320dffc7
kshitiz jain <kshtzkmljn@gmail.com>
2025-05-15 01:03:57 +0530

    Refactored and added the code

--- src/main/java/org/example/StringCal.java
+++ src/main/java/org/example/StringCal.java
@@ -2,7 +2,13 @@ package org.example;
 
 public class StringCal {
     public int stringCalculator(String str){
+        if(str == "") {
+            return 0;
+        }else if(str.length() == 1){
+            return Integer.valueOf(str);
+        }
 
-        return 0;
+        int ans = 0;
+       return ans;
     }
 }
