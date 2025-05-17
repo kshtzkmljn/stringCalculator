15711b8e246a07c5760cf59e95139e5f41ed88c5
kshitiz jain <kshtzkmljn@gmail.com>
2025-05-17 21:02:39 +0530

    Adding code for delimiter and corrected the test case

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,14 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string : ");
+        String input = sc.nextLine();
+        StringCal cal = new StringCal();
+        System.out.println(cal.stringCalculator(input));
     }
 
 
