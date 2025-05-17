139f08a77d1f9a091edecd862bf6101ede7ecadc
kshitiz jain <kshtzkmljn@gmail.com>
2025-05-17 19:03:24 +0530

    Adding test and code for new line

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -18,8 +18,13 @@ public class MainTest{
     }
 
     @Test
-    void tetsAddingTwoNumbers(){
+    void testAddingTwoNumbers(){
         assertEquals(cal.stringCalculator("1,2"),3);
+    }
+
+    @Test
+    void handleNewLineBetweenTwoNum(){
+        assertEquals(cal.stringCalculator("1\n2,3"),6);
     }
 
 
